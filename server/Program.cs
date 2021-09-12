38e9c5060028da34bd06aa2c3fadead0360faae0
Henrique Andrade <henrique.andrade22@hotmail.com>
2021-09-12 17:56:59 -0300

    initial commit

---
+++ server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            Server server = new Server(ipAddress);
+
+
+
+            server.Start();
+        }
+    }
+}
